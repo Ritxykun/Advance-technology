82d98fe28e1eac2ce1ec8eba85e16dc0c691c589
Ritxykun <72078825+Ritxykun@users.noreply.github.com>
2020-10-11 16:29:09 +0100

    Add files via upload

---
+++ Question.py
@@ -0,0 +1,4 @@
+class Question:
+    def __init__(self, promt, anwser):
+        self.promt= promt
+        self.anwser= anwser
---
+++ random.py
@@ -0,0 +1,8 @@
+import random
+
+nouns = ("puppy", "car", "rabbit", "girl", "monkey")
+verbs = ("runs", "hits", "jumps", "drives", "barfs") 
+adv = ("crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally.")
+adj = ("adorable", "clueless", "dirty", "odd", "stupid")
+num = random.randrange(0,5)
+print (nouns[num])
